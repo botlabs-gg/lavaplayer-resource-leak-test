78a2fec0cf6f86c4b0e013d9a67fbeb621a93ec7
Freya Arbjerg <freya.arbjerg@bluestacks.com>
2024-04-01 12:42:29 +0200

    Refactor PlaylistLoader

--- src/main/kotlin/PlaylistLoader.kt
+++ src/main/kotlin/PlaylistLoader.kt
@@ -5,54 +5,37 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack
+import java.util.concurrent.CountDownLatch
 
-
-@Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
 class PlaylistLoader(val apm: AudioPlayerManager) : AudioLoadResultHandler {
     private var exception: RuntimeException? = null
     private val results: MutableList<AudioTrack> = mutableListOf()
+    private val latch = CountDownLatch(1)
 
     fun loadTracksSync(identifier: String): List<AudioTrack>? {
         apm.loadItem(identifier, this)
-
-        try {
-            synchronized(this) {
-                (this as Object).wait()
-            }
-        } catch (e: InterruptedException) {
-            throw RuntimeException(e)
-        }
-
+        latch.await()
         exception?.let { throw it }
-
         return results
     }
 
     override fun trackLoaded(track: AudioTrack) {
         results.add(track)
-        synchronized(this) {
-            (this as Object).notify()
-        }
+        latch.countDown()
     }
 
     override fun playlistLoaded(playlist: AudioPlaylist) {
         results.addAll(playlist.tracks)
-        synchronized(this) {
-            (this as Object).notify()
-        }
+        latch.countDown()
     }
 
     override fun noMatches() {
         exception = RuntimeException("No matches")
-        synchronized(this) {
-            (this as Object).notify()
-        }
+        latch.countDown()
     }
 
     override fun loadFailed(exception: FriendlyException) {
         this.exception = exception
-        synchronized(this) {
-            (this as Object).notify()
-        }
+        latch.countDown()
     }
 }
